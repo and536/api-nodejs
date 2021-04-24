180f7ad1df1d4afa2c725fc17673ca71186d6c66
Diego Jesus <diego.jesus@projetas.com.br>
2021-04-23 22:13:11 -0300

    retirando string connection do codigo

--- src/infra/env.ts
+++ src/infra/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://admin:admin2020@cluster0-vq3ea.mongodb.net/test?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL || '',
   dataBaseName: 'DBDatahub'
 }
